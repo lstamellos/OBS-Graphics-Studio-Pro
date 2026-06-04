59cca7752e94e746888f4d2f4ccce8baf4de50ba
lstamellos <lstamellos@gmail.com>
2026-06-04 14:13:18 +0300

    Refine text character controls layout

--- src/title-source.cpp
+++ src/title-source.cpp
@@ -472,8 +472,11 @@ static QFont font_for_layer(const Layer &layer)
         font.setStyleName(style);
     font.setBold(layer.font_bold);
     font.setItalic(layer.font_italic);
-    font.setKerning(layer.font_kerning);
-    font.setLetterSpacing(QFont::AbsoluteSpacing, layer.char_tracking);
+    font.setUnderline(layer.text_underline);
+    font.setStrikeOut(layer.text_strikethrough);
+    font.setKerning(layer.kerning_mode != 2 && layer.font_kerning);
+    const float effective_tracking = layer.char_tracking + (layer.kerning_mode == 2 ? layer.manual_kerning : 0.0f);
+    font.setLetterSpacing(QFont::AbsoluteSpacing, effective_tracking);
     font.setStretch(std::clamp((int)std::round(layer.char_scale_x * 100.0f), 1, 4000));
     apply_text_style_to_font(font, layer);
     return font;
@@ -743,6 +746,8 @@ static void render_layer_text(cairo_t *cr, const Layer &layer, double t,
         ? ticker_text_path(font, text_rect, align, text, layer)
         : text_overflow_path(font, text_rect, align, text, layer);
     text_path = apply_vertical_character_scale(text_path, text_rect, align, layer);
+    if (std::abs(layer.baseline_shift) > 0.0001)
+        text_path.translate(0.0, -layer.baseline_shift);
 
     if (eval_shadow_enabled(layer, t)) {
         QColor shadow = color_from_argb(eval_shadow_color(layer, t));
