c35c18ba864cd9355a42fb40290920f8a008600e
lstamellos <lstamellos@gmail.com>
2026-06-04 08:56:39 +0300

    Add live cue header selection controls

--- src/title-dock.h
+++ src/title-dock.h
@@ -51,6 +51,7 @@ private slots:
     void on_add_to_scene();
     void on_selection_changed();
     void on_add_live_text_row();
+    void on_delete_live_text_rows();
     void on_move_live_text_row_up();
     void on_move_live_text_row_down();
 
@@ -58,10 +59,13 @@ private:
     void build_ui();
     void populate_list();
     void populate_exposed_text();
+    void set_all_live_text_rows_checked(bool checked);
+    void update_live_text_select_all_state();
     std::string selected_id() const;
     std::shared_ptr<Title> create_template_title(const std::string &name, int template_id);
     void select_title(const std::string &id);
     void create_title_from_template(const std::string &name, int template_id);
+    std::vector<int> selected_live_text_rows() const;
 
     QWidget      *container_  = nullptr;
     QListWidget  *list_       = nullptr;
@@ -77,6 +81,7 @@ private:
     QLabel       *text_editor_lbl_ = nullptr;
     QTableWidget *text_table_ = nullptr;
     QToolButton *btn_add_text_row_ = nullptr;
+    QToolButton *btn_delete_text_row_ = nullptr;
     QToolButton *btn_row_up_ = nullptr;
     QToolButton *btn_row_down_ = nullptr;
     bool          updating_exposed_text_ = false;
